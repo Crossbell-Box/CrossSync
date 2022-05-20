5873572ef86b3b471b00bca56c27def26471f127
Nya Candy <dev@candinya.com>
2022-05-20 16:29:10 +0800

    feat: settings page

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -3,18 +3,58 @@
         <Sidebar />
         <div class="flex-1 flex flex-col">
             <h1 class="flex text-4xl font-bold my-5 py-4">Settings</h1>
+            <div class="flex flex-col">
+                <el-col class="flex flex-col gap-3">
+                    <el-card shadow="hover" class="cursor-pointer" @click="toEditProfile">
+                        <div class="flex flex-row" style="justify-content: space-between">
+                            <div class="flex">Edit Profile</div>
+                            <div class="flex w-4">
+                                <ArrowRight />
+                            </div>
+                        </div>
+                    </el-card>
+                    <el-card shadow="hover" class="cursor-pointer" @click="logout">
+                        <div class="flex flex-row" style="justify-content: space-between">
+                            <div class="flex">Logout</div>
+                            <div class="flex w-4">
+                                <ArrowRight />
+                            </div>
+                        </div>
+                    </el-card>
+                </el-col>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { useStore } from '@/common/store';
 import Sidebar from '@/components/Sidebar.vue';
+import { ArrowRight } from '@element-plus/icons-vue';
+import { disconnect } from '@/common/wallet';
+import { ElMessageBox } from 'element-plus';
 
 const router = useRouter();
 const store = useStore();
+
+const toEditProfile = () => {
+    router.push('/profiles');
+};
+
+const logout = () => {
+    ElMessageBox.confirm('Are you sure to logout?', 'Logout', {
+        confirmButtonText: 'Yes',
+        cancelButtonText: 'No',
+        type: 'warning',
+    })
+        .then(async () => {
+            await disconnect();
+            await store.dispatch('reset');
+            await router.push('/');
+        })
+        .catch(() => {});
+};
 </script>
 
 <style></style>

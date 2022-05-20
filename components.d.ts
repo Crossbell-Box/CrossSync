c23ee0496ae5dbecc3203fd85593b46c8b0dc949
DIYgod <i@diygod.me>
2022-05-20 23:40:37 +0100

    chore: fix web3modal build error

--- components.d.ts
+++ components.d.ts
@@ -13,7 +13,6 @@ declare module '@vue/runtime-core' {
     ElInput: typeof import('element-plus/es')['ElInput']
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
-    ElPopconfirm: typeof import('element-plus/es')['ElPopconfirm']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     InfiniteScroll: typeof import('element-plus/es')['ElInfiniteScroll']
     Loading: typeof import('element-plus/es')['ElLoadingDirective']
--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,18 @@ import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 import path from 'path';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
+const production = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            plugins: [nodePolyfills()],
+        },
+        commonjsOptions: {
+            transformMixedEsModules: true,
+        },
+    },
     resolve: {
         alias: {
             '@': path.resolve(__dirname, 'src'),
@@ -23,5 +33,9 @@ export default defineConfig({
         Components({
             resolvers: [ElementPlusResolver()],
         }),
+        !production &&
+            nodePolyfills({
+                include: ['node_modules/**/*.js', new RegExp('node_modules/.vite/.*js')],
+            }),
     ],
 });

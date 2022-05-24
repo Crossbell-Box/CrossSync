b4aa12f570bebbec70118f1e87821da39bc28433
DIYgod <i@diygod.me>
2022-05-24 01:22:55 +0100

    feat: connect with MetaMask in extension env

--- src/common/wallet.ts
+++ src/common/wallet.ts
@@ -1,5 +1,6 @@
 import Web3Modal from 'web3modal';
 import WalletConnectProvider from '@walletconnect/web3-provider';
+import createMetaMaskProvider from 'metamask-extension-provider';
 
 const providerOptions = {
     walletconnect: {
@@ -19,23 +20,46 @@ const web3Modal = new Web3Modal({
     providerOptions, // required
 });
 
+let metaMaskProvider: ReturnType<typeof createMetaMaskProvider>;
+
 export async function connect(force = false) {
-    if (force) {
-        return await web3Modal.connect();
-    } else {
-        if (web3Modal.cachedProvider) {
-            try {
-                return await web3Modal.connect();
-            } catch (e) {
-                // console.log(e);
-                throw e;
-                // return null;
-            }
+    if (window.chrome?.runtime?.id) {
+        if (!metaMaskProvider) {
+            metaMaskProvider = createMetaMaskProvider();
         }
-        return null;
+
+        if (force) {
+            await metaMaskProvider.request({
+                method: 'wallet_requestPermissions',
+                params: [
+                    {
+                        eth_accounts: {},
+                    },
+                ],
+            });
+        }
+
+        return metaMaskProvider;
+    } else {
+        if (force) {
+            return await web3Modal.connect();
+        } else {
+            if (web3Modal.cachedProvider) {
+                try {
+                    return await web3Modal.connect();
+                } catch (e) {
+                    // console.log(e);
+                    throw e;
+                    // return null;
+                }
+            }
+            return null;
+        }
     }
 }
 
 export async function disconnect() {
-    await web3Modal.clearCachedProvider();
+    if (!window.chrome?.runtime?.id) {
+        await web3Modal.clearCachedProvider();
+    }
 }

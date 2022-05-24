e2d7767d5b5df68d1ef57c5a4cc7e54265ab236a
DIYgod <i@diygod.me>
2022-05-24 02:12:59 +0100

    chore: fix await build error

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
         commonjsOptions: {
             transformMixedEsModules: true,
         },
+        target: 'esnext',
     },
     resolve: {
         alias: {

61d097540572b0213675eb1925aeb0064e426783
DIYgod <i@diygod.me>
2022-05-25 01:05:47 +0100

    chore: update dependencies

--- src/content-script/index.ts
+++ src/content-script/index.ts
@@ -1,14 +1,12 @@
 import { observe } from './utils';
 import './locationChange';
 
-// import syncControl from '@/common/sync-control';
-
-import { hooks as twitterHooks } from './hooks/twitter';
+import hooks from './hooks/twitter';
 
 (async () => {
     switch (new URL(window.location.href).host) {
         case 'twitter.com':
-            twitterHooks.forEach((h) => {
+            hooks.forEach((h) => {
                 observe(h.selector, h.callback);
             });
             break;

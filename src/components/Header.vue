b600f5dd2b7e8c38886a7ea58b5589577e4fabb5
Nya Candy <dev@candinya.com>
2022-05-26 22:36:58 +0800

    feat: LOGO

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="absolute left-8 top-8">
         <router-link to="/">
-            <img class="inline-block rounded-full align-middle" width="50" height="50" src="../assets/logo.jpeg" />
+            <img class="inline-block rounded-full align-middle" width="50" height="50" src="../assets/logo.png" />
         </router-link>
     </div>
     <div class="absolute bottom-8 text-slate-400 text-sm -translate-x-1/2 left-1/2">
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,7 +2,7 @@
     <Header />
     <div class="text-center w-full h-full flex items-center justify-center">
         <div class="pb-8">
-            <img class="inline-block rounded-full" width="150" src="../assets/logo.jpeg" />
+            <img class="inline-block rounded-full" width="150" src="../assets/logo.png" />
             <h1 class="text-4xl font-bold my-5">Welcome to CrossSync! 👋</h1>
             <p class="my-5 text-slate-400">Here are some introductions to CrossSync and Crossbell...</p>
             <el-button type="primary" :loading="isConnecting" round size="large" class="text-xl" @click="connect"

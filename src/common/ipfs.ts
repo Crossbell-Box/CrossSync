6c798452973345c3f525d2c7ed146a9f47f6d117
Nya Candy <dev@candinya.com>
2022-05-30 10:24:58 +0800

    feat: use IPFS relay

--- src/common/ipfs.ts
+++ src/common/ipfs.ts
@@ -1,20 +1,14 @@
-const Web3StorageToken =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDNjRjEzMTJDNjM4ZDAwMWYyMmMwZkM5NzgxMzYwNjM1QkIxNGVBMjkiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NTM0NTU5NDM2MjksIm5hbWUiOiJDcm9zc1N5bmMifQ.h1jr-jHrRUxZhARzJQzuVBzEHVgu3atCS8VJAqtO4Hk';
-
 export const upload = async (file: File | Blob) => {
     const formData = new FormData();
     formData.append('file', file);
 
-    const result = await fetch('https://api.web3.storage/upload', {
-        method: 'POST',
-        headers: {
-            Authorization: `Bearer ${Web3StorageToken}`,
-        },
+    const result = await fetch('https://ipfs-relay.crossbell.io/upload', {
+        method: 'PUT',
         body: formData,
     });
 
     if (result.ok) {
-        const json = await result.json();
-        return `ipfs://${json.cid}`;
+        const res = await result.json();
+        return res.url;
     }
 };

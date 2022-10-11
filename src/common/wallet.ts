0d25d3fd6683b3b72d48f8642cc576fd7d905f3b
Nya Candy <dev@candinya.com>
2022-10-11 13:18:04 +0800

    feat: refractor wallet connect lib

--- src/common/wallet.ts
+++ src/common/wallet.ts
@@ -1,66 +1,35 @@
-import Web3Modal from 'web3modal';
-import WalletConnectProvider from '@walletconnect/web3-provider';
 import createMetaMaskProvider from 'metamask-extension-provider';
-
-const providerOptions = {
-    walletconnect: {
-        package: WalletConnectProvider,
-        options: {
-            rpc: {
-                3737: 'https://rpc.crossbell.io',
-            },
-            bridge: 'https://wcrv2.rss3.dev',
-        },
-    },
-};
-
-const web3Modal = new Web3Modal({
-    network: 'crossbell', // optional
-    cacheProvider: true, // optional
-    providerOptions, // required
-});
 
 let metaMaskProvider: ReturnType<typeof createMetaMaskProvider>;
 
 export async function connect(force = false) {
+    let metamask: any;
+
     if (!window.ethereum) {
         if (!metaMaskProvider) {
             metaMaskProvider = createMetaMaskProvider();
         }
-
-        if (force) {
-            await metaMaskProvider.request({
-                method: 'eth_requestAccounts',
-            });
-            await metaMaskProvider.request({
-                method: 'wallet_requestPermissions',
-                params: [
-                    {
-                        eth_accounts: {},
-                    },
-                ],
-            });
-        }
-
-        return metaMaskProvider;
+        metamask = metaMaskProvider;
     } else {
-        if (force) {
-            return await web3Modal.connect();
-        } else {
-            if (web3Modal.cachedProvider) {
-                try {
-                    return await web3Modal.connect();
-                } catch (e) {
-                    throw e;
-                }
-            }
-            return null;
-        }
+        // window.ethereum is metaMaskProvider
+        metamask = window.ethereum;
     }
+
+    if (force) {
+        await metamask.request({
+            method: 'eth_requestAccounts',
+        });
+        await metamask.request({
+            method: 'wallet_requestPermissions',
+            params: [
+                {
+                    eth_accounts: {},
+                },
+            ],
+        });
+    }
+
+    return metamask;
 }
 
-export async function disconnect() {
-    if (!window.chrome?.runtime?.id) {
-        await web3Modal.clearCachedProvider();
-    }
-}
+export async function disconnect() {}
